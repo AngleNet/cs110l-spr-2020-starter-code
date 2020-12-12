db0ddf9ff666abf6a4a5024482053fa459b403b6
anglenet <anglenet@sina.cn>
2020-12-12 09:11:25 +0000

    Add rust launch config

--- proj-1/deet/src/inferior.rs
+++ proj-1/deet/src/inferior.rs
@@ -2,7 +2,9 @@ use nix::sys::ptrace;
 use nix::sys::signal;
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::Pid;
+use std::os::unix::process::CommandExt;
 use std::process::Child;
+use std::process::Command;
 
 pub enum Status {
     /// Indicates inferior stopped. Contains the signal that stopped the process, as well as the
@@ -34,12 +36,20 @@ impl Inferior {
     /// Attempts to start a new inferior process. Returns Some(Inferior) if successful, or None if
     /// an error is encountered.
     pub fn new(target: &str, args: &Vec<String>) -> Option<Inferior> {
-        // TODO: implement me!
-        println!(
-            "Inferior::new not implemented! target={}, args={:?}",
-            target, args
-        );
-        None
+        let mut cmd = Command::new(target);
+        unsafe {
+            cmd.args(args).pre_exec(|| {
+                println!("in side");
+                return Ok(());
+            });
+        }
+        let child = cmd.args(args).spawn();
+        if child.is_err() {
+            return None;
+        }
+        Some(Inferior {
+            child: child.unwrap(),
+        })
     }
 
     /// Returns the pid of this inferior.
